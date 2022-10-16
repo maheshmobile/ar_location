bfb11357c8f83ce64b3ff3a958d432ae9114131d
maheshmobile <maheshiosadvocate@gmail.com>
2022-10-16 12:45:22 +0300

    Update gps.js

--- src/gps.js
+++ src/gps.js
@@ -1,12 +1,12 @@
-window.onload = () => {
+// window.onload = () => {
 
-    const alertDiv = document.getElementById("alert");
-    navigator.geolocation.watchPosition(function(pos) {
-        alertDiv.innerHTML = pos.coords.latitude + ", " + pos.coords.longitude + ", " + pos.coords.accuracy;
-        console.log(pos);
-    }, function(error) {
-        alertDiv.innerHTML = error.message;
-    }, {});
+//     const alertDiv = document.getElementById("alert");
+//     navigator.geolocation.watchPosition(function(pos) {
+//         alertDiv.innerHTML = pos.coords.latitude + ", " + pos.coords.longitude + ", " + pos.coords.accuracy;
+//         console.log(pos);
+//     }, function(error) {
+//         alertDiv.innerHTML = error.message;
+//     }, {});
     
-};
+// };
 
